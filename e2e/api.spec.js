ae6a6cb916b1ba9e17509218f964df8155b159e2
Vignesh <viki14395@gmail.com>
2024-11-19 22:37:07 +0530

    Initial commit: Playwright API test setup

---
+++ e2e/api.spec.js
@@ -0,0 +1,9 @@
+const { test, expect } = require('@playwright/test');
+
+test('GET API Test', async ({ request }) => {
+    const response = await request.get('/posts/1'); // Example endpoint
+    expect(response.status()).toBe(200);
+    const data = await response.json();
+    console.log(data);
+    expect(data).toHaveProperty('id', 1);
+});
