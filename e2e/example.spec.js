496bd94bc286f04b1b3ebfdb99064ac25369c1d8
imvkrish <113405571+imvkrish@users.noreply.github.com>
2024-12-04 09:05:56 +0530

    Update example.spec.js

--- e2e/example.spec.js
+++ e2e/example.spec.js
@@ -17,6 +17,7 @@ test('get started link', async ({ page }) => {
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
+makin some update here to check if thery are updated in my local
 
 
 removed***
